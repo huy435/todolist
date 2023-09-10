b0612c690bcd8c67d819ae052ce9f6c4fb78d6f4
hoprik <42morozov42@gmail.com>
2023-09-10 20:44:05 +0300

    todolist

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+let taski = ["task","task","task"]
+const tasks = document.querySelector(".tasks")
+const TextAdd = document.querySelector(".text-adding")
+const ButAdd = document.querySelector(".but-adding")
+
+function deleteTask(i) {
+    taski.splice(i, 1)
+    render()
+}
+
+ButAdd.addEventListener("click", ()=>{
+    taski.push(TextAdd.value)
+    render()
+})
+
+function render(params) {
+    tasks.innerHTML = ""
+    taski.forEach((task, i)=>{
+        tasks.insertAdjacentHTML('beforeend', `<div class="text-task">${task}</div>
+        <button class="btn-trash"><img onclick="deleteTask(${i})" class="trash" src="trash.svg" alt=""></button>
+    </div>
+    <div class="line"></div>`)
+
+    })
+}
+
+render()
 
